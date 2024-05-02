f9756e157965b219dfd5e127f130056a590f645e
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-05-02 12:40:57 +0500

    ---

--- checkCart.php
+++ checkCart.php
@@ -2,7 +2,21 @@
 session_start();
 $id_item = isset($_GET['item'])?$_GET['item']:false;
 if (!isset($_SESSION['arrItems'])) {$_SESSION['arrItems']=[];}
-array_push($_SESSION['arrItems'], $id_item);
-echo "<script>alert('Товар добавлен в корзину!');
+
+for ($i=0; $i<count($_SESSION['arrItems']); $i++)
+ {if ($_SESSION['arrItems'][$i]['id_item'] == $id_item) 
+    {$_SESSION['arrItems'][$i]['amount']++;
+    echo "<script>alert('Товар добавлен в корзину!');
               location.href='tovar.php?id_item=$id_item'; </script>";
+    exit();}
+ }
+//  $count = count($_SESSION['arrItems']);
+// array_push($_SESSION['arrItems'][$count]['amount'], 1);
+// array_push($_SESSION['arrItems'][$count]['id_item'], $id_item);
+array_push($_SESSION['arrItems'], ["id_item"=>$id_item, "amount"=>1]);
+    echo "<script>alert('Товар добавлен в корзину!');
+              location.href='tovar.php?id_item=$id_item'; </script>";
+
+// array_push($_SESSION['arrItems']['id_item'], $id_item);
+
 ?>
--- exit.php
+++ exit.php
@@ -1,6 +1,7 @@
 <?php 
 session_start();
 unset($_SESSION['id']); 
+unset($_SESSION['arrItems']); 
 // setcookie('user', $user['id'], time() - 3600, "/");
 header("Location: catalogue.php ");
 
